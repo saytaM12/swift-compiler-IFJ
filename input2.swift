71cf5171fe4ceba62800776a0311d5a46707e440
saytaM12 <matyas.oujezdsky@gmail.com>
2023-12-06 21:49:38 +0100

    j

--- input2.swift
+++ input2.swift
@@ -1,5 +1,6 @@
 // Program 1: Vypocet faktorialu (iterativne)
 /* Hlavni telo programu */
+/*
 func hello(from x: Int, to y: Int) {
     return 0
     write("Zadejte cislo pro vypocet faktorialu\n")
@@ -8,6 +9,10 @@ let b : Int = 6
 let a : Int = 4
 
 hello(a, b)
+ */
+let b : Int = 6
+let a : Int = 5
+let c : Int = 5 + 6
 /*
 if let a {
     hello(from: a, to: b)
